cf0120ceb5fb501b4d355173879f79b1946de339
Tom Walker <37417736+tomjwalker@users.noreply.github.com>
2023-12-14 06:22:42 +0000

    refactor: argmax fn to utils

--- utils/general.py
+++ utils/general.py
@@ -1,5 +1,7 @@
 import os
 
+import numpy as np
+
 
 def set_filepath(filepath):
     """
@@ -9,3 +11,27 @@ def set_filepath(filepath):
     if not os.path.exists(os.path.dirname(filepath)):
         os.makedirs(os.path.dirname(filepath))
     return filepath
+
+
+def argmax(q_values):
+    """
+    Gets the argmax of the q-values. Splits ties randomly.
+
+    Args:
+        q_values (np.ndarray): The q-values for each action.
+
+    Returns:
+        int: The action to take.
+    """
+    top = float("-inf")
+    ties = []
+
+    for i in range(len(q_values)):
+        if q_values[i] > top:
+            top = q_values[i]
+            ties = []
+
+        if q_values[i] == top:
+            ties.append(i)
+
+    return np.random.choice(ties)
