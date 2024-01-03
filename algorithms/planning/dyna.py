786947b3b0e7a07e89865f3461de7026f618c346
Tom Walker <37417736+tomjwalker@users.noreply.github.com>
2024-01-03 13:48:29 +0000

    dyna working

--- algorithms/planning/dyna.py
+++ algorithms/planning/dyna.py
@@ -1,34 +1,63 @@
 # Import custom argmax function
+import pandas as pd
+
 from utils.general import argmax
 
-import gymnasium as gym
+# Import relevant gridworld environment (local implementation of gridworld pp165 of Sutton and Barto (2018))
+from environment.planning_maze import Maze
+#
+# import gymnasium as gym
 import numpy as np
+from collections import defaultdict    # For model, with default value of empty list
+import random    # For random choice of state and action from model during planning
 
 import matplotlib
 from matplotlib import pyplot as plt
 matplotlib.use('TkAgg')
 
 
-class QLearning:
+# def make_env():
+#     custom_map = [
+#         "FFFFFFFHG",
+#         "FFHFFFFHF",
+#         "SFHFFFFHF",
+#         "FFHFFFFFF",
+#         "FFHFFFFFF",
+#         "FFFFFFFFF",
+#         ]
+#     env = gym.make("FrozenLake-v1", desc=custom_map, is_slippery=False)
+#     return env
 
-    def __init__(self, env, alpha=0.5, gamma=1.0, epsilon=0.1):
-        self.name = "Q-Learning"
+
+class Dyna:
+
+    def __init__(self, env, alpha=0.5, gamma=1.0, epsilon=0.1, n_planning_steps=5):
+        self.name = "Dyna"
         self.env = env
         self.alpha = alpha
         self.gamma = gamma
         self.epsilon = epsilon
+        self.n_planning_steps = n_planning_steps
 
         self.q_values = None
+        self.model = None
         self.policy = None
         self.episode_rewards = None
+        self.episode_steps = None
         self.reset()
 
     def reset(self):
-        self.q_values = np.zeros((self.env.observation_space.n, self.env.action_space.n))
+        self.q_values = np.zeros(shape=(self.env.observation_space.n, self.env.action_space.n))
         self.policy = np.zeros_like(self.q_values)
         self.episode_rewards = []  # Stores total reward for each episode
+        self.episode_steps = []  # Stores total steps for each episode
+
+        # Model is a dictionary of lists, where each list contains tuples of (reward, next_state), and the key is the
+        # #(state, action) pair
+        self.model = defaultdict(list)
 
     def act(self, state):
+        """Given a state, return an action according to the epsilon-greedy policy."""
         if np.random.random() < self.epsilon:
             return self.env.action_space.sample()
         else:
@@ -38,37 +67,71 @@ class QLearning:
 
         for episode in range(num_episodes):
 
-            # Initialise S
+            # Initialise S (**a**)
+            #TODO: THIS NEEDS TO BE FIXED!!!
             state, _ = self.env.reset()
 
-            # Initialise reward counter
+            # Initialise logging variables
             episode_reward = 0
+            episode_steps = 0
 
             # Loop over each step of episode, until S is terminal
             done = False
             while not done:
 
-                # Choose A from S using policy derived from Q (epsilon-greedy)
+                # Choose A from S using policy derived from Q (epsilon-greedy) (**b**)
                 action = self.act(state)
 
-                # Take action A, observe R, S'
+                # Take action A, observe R, S' (**c**)
                 next_state, reward, terminated, truncated, _ = self.env.step(action)
 
-                # Update Q(S, A), taking as target the q-learning TD target (R + gamma * max_a Q(S', a))
-                target = reward + self.gamma * self.q_values[next_state, argmax(self.q_values[next_state, :])]
-                self.q_values[state][action] += self.alpha * (target - self.q_values[state][action])
+                # Update Q(S, A) (**d**)
+                td_target = reward + self.gamma * np.max(self.q_values[next_state, :])
+                self.q_values[state][action] += self.alpha * (td_target - self.q_values[state][action])
 
-                # S <- S', A <- A'
-                state = next_state
-
-                # Add reward to episode reward
+                # Update logs
                 episode_reward += reward
+                episode_steps += 1
+
+                # Update model (**e**).
+                # Model is a dictionary: {(state, action): [(reward, next_state)_1, (reward, next_state)_2]}
+                # If (reward, next_state) is not in the list, add it, otherwise do nothing
+                if (state, action) not in self.model:
+                    self.model[(state, action)].append((reward, next_state))
+                elif (reward, next_state) not in self.model[(state, action)]:
+                    self.model[(state, action)].append((reward, next_state))
+
+                # TODO: temp: remove
+                if episode == 2:
+                    print("debug")
+
+                # Loop for n planning steps, and perform planning updates (**f**)
+                for _ in range(self.n_planning_steps):
+
+                    print(f"Episode {episode}, planning step {_}")
+
+                    # Choose a random, previously observed state and action (**f.i, f.ii**)
+                    (state, action) = random.choice(list(self.model.keys()))
+
+                    # TODO: check want to retain this line
+                    # Get reward and next state from model (N.B. unlike S&B, this accounts for stochasticity in the
+                    # environment) (**f.iii**)
+                    # Make sure next state from learning is not mixed up with next state from planning (for S <- S')
+                    (reward, next_state_plan) = random.choice(self.model[(state, action)])
+
+                    # Update Q(S, A), taking as target the q-learning TD target (R + gamma * max_a Q(S', a)) (**f.iv**)
+                    td_target = reward + self.gamma * np.max(self.q_values[next_state_plan, :])
+                    self.q_values[state][action] += self.alpha * (td_target - self.q_values[state][action])
+
+                # S <- S'
+                state = next_state
 
                 # If S is terminal, then episode is done (exit loop)
                 done = terminated or truncated
 
-            # Add episode reward to list
+            # Update logs
             self.episode_rewards.append(episode_reward)
+            self.episode_steps.append(episode_steps)
 
             if episode % 100 == 0:
                 print(f"Episode {episode}")
@@ -76,17 +139,17 @@ class QLearning:
                 print()
 
 
-def plot_episode_rewards(episode_rewards, title):
+def plot_episode_steps(episode_steps, title):
     """
     Plot the episode rewards.
     """
     # Plot the episode rewards
     fig, ax = plt.subplots(figsize=(10, 5))
-    ax.plot(episode_rewards)
+    ax.plot(episode_steps)
     ax.set_xlabel("Episode")
-    ax.set_ylabel("Episode reward")
+    ax.set_ylabel("Episode steps")
     # Set y-limits
-    ax.set_ylim([-100, 0])
+    # ax.set_ylim([0, 200])
     ax.set_title(title)
     plt.show()
 
@@ -94,15 +157,33 @@ def plot_episode_rewards(episode_rewards, title):
 def run():
 
     # Run parameters
-    train_episodes = 1000
+    train_episodes = 50
+    gamma = 0.95
+    run_specs = {
+        "planning steps": [0, 5, 50],
+        "colour": ["blue", "green", "red"],
+        "label": ["0 planning steps (direct RL)", "5 planning steps", "50 planning steps"],
+    }
+    run_specs = pd.DataFrame(run_specs)
 
-    # Create the environment
-    env = gym.make("CliffWalking-v0")
-    rl_loop = QLearning(env)
-    rl_loop.train(num_episodes=train_episodes)
+    for i, row in run_specs.iterrows():
 
-    # Plot the results
-    plot_episode_rewards(rl_loop.episode_rewards, rl_loop.name)
+        # Create the environment
+        env = Maze()
+
+        # Create and train the agent
+        rl_loop = Dyna(env, gamma=gamma, n_planning_steps=row["planning steps"])
+        rl_loop.train(num_episodes=train_episodes)
+
+        # Plot the results
+        plt.plot(rl_loop.episode_steps, color=row["colour"], label=row["label"])
+
+    plt.ylim(bottom=0, top=800)    # Set y-limits after all plots are generated
+    plt.xlabel("Episode")
+    plt.ylabel("Episode steps")
+    plt.title(f"Episode steps for Dyna agent (gamma={gamma})")
+    plt.legend()
+    plt.show()
 
 
 if __name__ == "__main__":
