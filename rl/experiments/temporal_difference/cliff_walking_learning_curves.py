c09ddb9f23f5966f81c706136ec660df7d2c9d4b
Tom Walker <37417736+tomjwalker@users.noreply.github.com>
2024-09-26 06:41:58 +0100

    new section for state visit viz; unsure it currently works; TODO

--- rl/experiments/temporal_difference/cliff_walking_learning_curves.py
+++ rl/experiments/temporal_difference/cliff_walking_learning_curves.py
@@ -1,14 +1,18 @@
 # TODO: hyperparameter sweep as pp. 133 of Sutton and Barto
+# TODO: state visitation count plots
 
 
 from rl.algorithms.temporal_difference.q_learning import QLearning
 from rl.algorithms.temporal_difference.expected_sarsa import ExpectedSarsa
 from rl.algorithms.temporal_difference.sarsa import Sarsa
 from rl.simulation.trial import Trial
-from rl.environment.temporal_difference.cliff_walking_utils import visualise_q    # Specific visualisation function for cliff walking    # NoQA
+from rl.environment.temporal_difference.cliff_walking_utils import visualise_q
+# from rl.environment.temporal_difference.cliff_walking_utils import visualise_state_visits    # Specific visualisation
+# function for cliff walking    # NoQA
 import gymnasium as gym
 import numpy as np
 from matplotlib import pyplot as plt
+
 
 # Enable interactive mode
 plt.ion()
@@ -20,6 +24,10 @@ episodes_per_session = 500
 
 # Set up the environment
 env = gym.make("CliffWalking-v0")
+
+# Show state visits plot flag (doubles the calculation time)
+# TODO: fold in state visit method to current approach
+show_state_visits = False
 
 # Set up the agents
 agents = {"Sarsa": (Sarsa, "blue"), "Q-Learning": (QLearning, "red"), "Expected Sarsa": (ExpectedSarsa, "green")}
@@ -65,6 +73,14 @@ fig, ax = plt.subplots(len(agents), 1, figsize=(10, 5 * len(agents)))
 for i, (agent_name, _) in enumerate(agents.items()):
     visualise_q(trained_agent_last_run[agent_name], ax=ax[i])
 plt.show()
+
+if show_state_visits:
+
+    # TODO: check this is fully implemented correctly
+
+    # Track state visitations
+    state_visits = {agent_name: np.zeros(env.observation_space.n) for agent_name in agents}
+
 
 # Keep the plot open
 plt.ioff()
