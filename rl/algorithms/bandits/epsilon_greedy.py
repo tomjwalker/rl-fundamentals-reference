70d5c8b437c4fec5ba547f6eb98c106ed3d3195b
Tom Walker <37417736+tomjwalker@users.noreply.github.com>
2024-10-02 17:02:27 +0100

    refactored to have plotting fn in separate module

--- rl/algorithms/bandits/epsilon_greedy.py
+++ rl/algorithms/bandits/epsilon_greedy.py
@@ -1,67 +1,16 @@
 # epsilon_greedy.py
 
-from typing import Tuple, Dict, List
+from typing import Tuple
 import numpy as np
 import pandas as pd
-from scipy import stats
 import matplotlib.pyplot as plt
 import matplotlib
 
+from rl.algorithms.bandits.viz import plot_trial_results
 from rl.environment.bandits.k_armed_bandit import KArmedTestbed
 from rl.utils.general import argmax_ties_random
 
 matplotlib.use('TkAgg')
-
-
-def plot_trial_results(
-    rewards_testbed: pd.DataFrame,
-    optimal_action_testbed: pd.DataFrame,
-    params: Dict[str, float],
-    ax: List[plt.Axes],
-    color: str,
-    show_individual_runs: bool = False,
-    show_confidence_interval: bool = False
-) -> None:
-    """
-    Plot the results of the trials for a given set of parameters.
-
-    Args:
-        rewards_testbed (pd.DataFrame): DataFrame containing rewards for each step and run.
-        optimal_action_testbed (pd.DataFrame): DataFrame indicating if the optimal action was taken.
-        params (Dict[str, float]): Parameters used in the experiment.
-        ax (List[plt.Axes]): List of matplotlib axes to plot on.
-        color (str): Color to use for plotting.
-        show_individual_runs (bool): Whether to plot individual runs.
-        show_confidence_interval (bool): Whether to show the confidence interval.
-    """
-    num_runs = rewards_testbed.shape[1]
-    steps = rewards_testbed.shape[0]
-
-    # Plot individual runs if specified
-    alpha = 0.15
-    if show_individual_runs:
-        for i in range(num_runs):
-            ax[0].plot(rewards_testbed.iloc[:, i], color=color, alpha=alpha)
-            ax[1].plot(optimal_action_testbed.iloc[:, i] * 100, color=color, alpha=alpha)
-
-    # Calculate mean
-    mean_rewards = rewards_testbed.mean(axis=1)
-    mean_optimal = optimal_action_testbed.mean(axis=1) * 100
-
-    # Plot mean
-    ax[0].plot(mean_rewards, color=color, linewidth=2, label=f"init={params['init']}")
-    ax[1].plot(mean_optimal, color=color, linewidth=2, label=f"init={params['init']}")
-
-    # Calculate and plot confidence interval if specified
-    if show_confidence_interval:
-        std_rewards = rewards_testbed.std(axis=1)
-        ci_rewards = stats.t.interval(0.95, num_runs - 1, loc=mean_rewards, scale=std_rewards / np.sqrt(num_runs))
-
-        std_optimal = optimal_action_testbed.std(axis=1) * 100
-        ci_optimal = stats.t.interval(0.95, num_runs - 1, loc=mean_optimal, scale=std_optimal / np.sqrt(num_runs))
-
-        ax[0].fill_between(range(steps), ci_rewards[0], ci_rewards[1], color=color, alpha=0.3)
-        ax[1].fill_between(range(steps), ci_optimal[0], ci_optimal[1], color=color, alpha=0.3)
 
 
 class EpsilonGreedy:
@@ -327,7 +276,7 @@ if __name__ == "__main__":
     # Uncomment one of the following lines to run the experiment with desired options:
 
     # Run with default settings
-    # initial_val_experiment()
+    initial_val_experiment()
 
     # Run showing individual runs
     # initial_val_experiment(show_individual_runs=True)
